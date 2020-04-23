3ae37d374a58aa5604dec5e70504915caff4241c
Peter Bolfa <xbolfa@fi.muni.cz>
2020-04-23 10:34:33 +0200

    Added model/IRoom

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,19 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import items from "./data";
+import { IRoom } from "./model/IRoom";
 
 const RoomContext = React.createContext<string>("");
 
-export default class RoomProvider extends Component {
-    state = {
+interface IProviderState {
+    rooms : IRoom[];
+    sortedRooms : IRoom[];
+    featuredRooms : IRoom[];
+    loading: boolean;
+}
 
-    }
-    render() {
-        return (
-            <RoomContext.Provider value ={"hello"}>
-            {this.props.children}
-            </RoomContext.Provider>
-        )
-    }
+interface IProviderProps {}
+
+export default class RoomProvider extends Component<IProviderProps, IProviderState> {
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    let rooms : IRoom[] = this.formatData(items);
+    
+    console.log(rooms);
+  }
+
+  formatData(items: any) : IRoom[] {
+    let tempItems : IRoom[] = items.map((item: any) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(
+        (image: any) => image.fields.file.url
+      );
+      let room = {...item.fields, images};
+      return room;
+    });
+    return tempItems;
+  }
+  render() {
+    return (
+      <RoomContext.Provider value={"hello"}>
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
 }
 
 const RoomConsumer = RoomContext.Consumer;
-export {RoomProvider, RoomConsumer, RoomContext};
+export { RoomProvider, RoomConsumer, RoomContext };
---
+++ src/model/IRoom.tsx
@@ -0,0 +1,14 @@
+export interface IRoom {
+    breakfast: boolean;
+    capacity: number;
+    description: string;
+    extras: string[];
+    featured: boolean;
+    images: string[];
+    name: string;
+    pets: false;
+    price: number;
+    size: number;
+    slug: string;
+    type: string;
+}
